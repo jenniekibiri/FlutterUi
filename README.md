8a1e853228231d36ee3ce6c7de29115f41471690
jenniekibiri <josphatje@gmail.com>
2020-09-29 09:15:31 +0300

    add rows,columns and stack

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ statlesswidget makes the app a widget and help in hot reload.
 scaffold -->its widget which allows us to set items on the app.
 
 its like a container or a wrapper.
-it can take app bar,body.
+it can take app bar,body,title
 visit scaffold classes
 
 widgets provides build method.
@@ -25,5 +25,23 @@ use the build we intialized instead of the one inherited from the parent
 # importing third party packages like fonts 
 download the font aand import it in the pubsec
 # columns
+columns have children which take  up multiple widgets
+use mainaxisalignment property to and space around
+and crossaxisalignment to control to x direction
+
+# rows
+rows have children <widget>.
+which takes multiple widgets.
+to control space in the rows use the 
+mainAxisalignment property or the crossaxisalignment property
+
+# stack 
+overlapping widgets 
+
+# box decoration
+provides diffent ways to draw a box
+
+# expanded
+makes the row,column or flex fill the available space along the main axiz
 
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 
-//import the flutter package first
+
 void main() => runApp(QuickBee());
-// call the  class youll have your app on
-//what is override?
-//the scaffold is like a container
 
 class QuickBee extends StatelessWidget {
   @override
@@ -13,23 +10,156 @@ class QuickBee extends StatelessWidget {
     MaterialApp(
       title: 'QuickBee',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(fontFamily: "Roboto"),
+
+      theme: ThemeData(fontFamily: "Roboto"
+      ),
+
       home: MyHomePage()
     );
   }
 }
+
 
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return (
     Scaffold(
-        appBar: AppBar(
-          title: Text('flutter app'),
-        ),
-        body:  Center(
-          child: const Text('Hello World'),
-        ),
+        body: Center(
+          child:Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children:<Widget> [
+            Stack(
+              alignment: Alignment.center,
+             children: <Widget>[
+                Container(
+               
+              height: 60.0,
+              width: 60.0,
+              decoration:BoxDecoration(
+                color: Color(0xFF18D191),
+                borderRadius: BorderRadius.circular(50.0)
+             ),
+              child:Icon(
+                Icons.local_offer,
+                color: Colors.white,
+              )
+            ),
+            Container(
+               margin: EdgeInsets.only(right:50.0 ,top: 50.0),
+              height: 60.0,
+              width: 60.0,
+              decoration:BoxDecoration(
+                color: Color(0xFFFC6A7F),
+                borderRadius: BorderRadius.circular(50.0)
+             ),
+              child:Icon(
+                Icons.home,
+                color: Colors.white,
+              )
+            ),
+            Container(
+                margin: EdgeInsets.only(left:30.0 ,top: 50.0),
+              height: 60.0,
+              width: 60.0,
+              decoration:BoxDecoration(
+                color: Color(0xFFFFCE56),
+                borderRadius: BorderRadius.circular(50.0)
+             ),
+              child:Icon(
+                Icons.local_car_wash,
+                color: Colors.white,
+              )
+            ),
+              Container(
+                margin: EdgeInsets.only(left:90.0 ,top: 50.0),
+              height: 60.0,
+              width: 60.0,
+              decoration:BoxDecoration(
+                color: Color(0xFF45E0EC),
+                borderRadius: BorderRadius.circular(50.0)
+             ),
+              child:Icon(
+                Icons.place,
+                color: Colors.white,
+              )
+            )
+             ],
+            ),
+            Row(
+
+            mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+             padding: EdgeInsets.only(top: 8.0,bottom: 8.0),
+             child: Text(
+               "Quick Bee",
+               style: TextStyle(fontSize: 30.0),
+               ),
+
+                )
+            ],
+            ),
+              Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        left: 20.0, right: 20.0, top: 10.0),
+                      child: Container(
+                          alignment: Alignment.center,
+                          height: 60.0,
+                          decoration: new BoxDecoration(
+                              color: Color(0xFF18D191),
+                              borderRadius: new BorderRadius.circular(9.0)),
+                          child: new Text("Sign In With Email",
+                              style: new TextStyle(
+                                  fontSize: 20.0, color: Colors.white))),
+                    ),
+                  ),
+                
+              ],
+            ),
+             new Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        left: 20.0, right: 5.0, top: 10.0),
+                    child: new Container(
+                        alignment: Alignment.center,
+                        height: 60.0,
+                        decoration: new BoxDecoration(
+                            color: Color(0xFF4364A1),
+                            borderRadius: new BorderRadius.circular(9.0)),
+                        child: new Text("FaceBook",
+                            style: new TextStyle(
+                                fontSize: 20.0, color: Colors.white))),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        left: 10.0, right: 20.0, top: 10.0),
+                    child: new Container(
+                        alignment: Alignment.center,
+                        height: 60.0,
+                        decoration: new BoxDecoration(
+                            color: Color(0xFFDF513B),
+                            borderRadius: new BorderRadius.circular(9.0)),
+                        child: new Text("Google",
+                            style: new TextStyle(
+                                fontSize: 20.0, color: Colors.white))),
+                  ),
+                )
+              ],
+            )
+            ],
+            ),
+            ),
+        
       )
     );
   }
